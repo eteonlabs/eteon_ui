27fe5c3463aefc3ff5f24230dfcfc2f6d8c81d36
Michael Knauer <knauer.michael@icloud.com>
2024-07-23 23:20:23 +0200

    Added AspectRatio to Documentation

---
+++ src/stories/AspectRatio.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { AspectRatio } from '@etn/components';
+
+const meta = {
+    title: 'Components/AspectRatio',
+    component: AspectRatio,
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<typeof AspectRatio>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {},
+    render: (args) => (
+        <AspectRatio
+            ratio={16 / 9}
+            className='bg-muted'
+        >
+            <img
+                src='https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80'
+                height={'250px'}
+                width={'5000px'}
+                className='rounded-md object-cover'
+            />
+        </AspectRatio>
+    ),
+};
